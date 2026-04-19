4b1dd98791faea7934d3a0fe1203b74dac78ecd4
Shubham Kumar <shubhammisra800@gmail.com>
2026-04-19 11:04:49 +0530

    update title and all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,15 @@ export class AppComponent {
 constructor(private title: Title, private meta: Meta) {}
 
 ngOnInit() {
-  this.title.setTitle('Web Development Services in Pune');
+  this.title.setTitle('Deep Cleaning Services in Pune | Home, Sofa & Office Cleaning - APK Elite Services|Affordable Deep Cleaning Services in Pune');
 
   this.meta.updateTag({
     name: 'description',
-    content: 'Professional web development services in Pune by APK Elite Services.'
+    content: 'Professional deep cleaning services in Pune by APK Elite Services. We offer home cleaning, sofa cleaning, bathroom and office cleaning at affordable prices. Book now!'
   });
+  this.meta.updateTag({
+  name: 'keywords',
+  content: 'affordable deep cleaning services in Pune, best home cleaning services in Pune, professional cleaning services in Pune, sofa cleaning services in Pune, bathroom deep cleaning Pune, office cleaning company Pune, carpet cleaning Pune, mattress cleaning Pune, water tank cleaning Pune, move in move out cleaning Pune, same day cleaning Pune'
+});
 }
 }
